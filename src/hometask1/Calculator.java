995fb4afda35a7e5507a7a1bc9cd86ab5f13bfdb
Sauron228 <burkovskiy_1997@ukr.net>
2018-12-22 23:25:27 +0300

    унпе

--- src/hometask1/Calculator.java
+++ src/hometask1/Calculator.java
@@ -1,72 +1,74 @@
 package hometask1;
 
 public class Calculator {
-    public static int add(int x, int y) {
+    public static void main(String[] args) {
         int result = x + y;
         return result;
+        public static int add ( int x, int y){
+            return x + y;
+        }
+
+        public static float add ( float x, float y){
+            return x + y;
+
+        }
+
+        public static double add ( double x, double y){
+            return x + y;
+
+        }
+
+        public static int sub (6, 3){//3
+            return x - y;
+        }
+
+        public static float sub (,float y){
+            return x - y;
+        }
+
+        public static double sub ( double x, double y){
+            return x - y;
+
+        }
+
+
+        public static int div ( int x, int y){
+            return x / y;
+        }
+
+        public static float div ( float x, float y){
+            return x / y;
+        }
+
+        public static double div ( double x, double y){
+            return x / y;
+
+        }
+
+        public static int mul ( int x, int y){
+            return x * y;
+        }
+
+        public static float mul ( float x, float y){
+            return x * y;
+        }
+
+        public static double mul ( double x, double y){
+            return x * y;
+
+        }
+
+        public static int main ( int x, int y){
+            return x + y;
+        }
+
+        public static double main ( double x, double y){
+            return x + y;
+        }
+
+        public static float main ( float x, float y){
+            return x + y;
+        }
     }
-
-    public static float add(float x, float y) {
-        return x + y;
-
-    }
-
-    public static double add(double x, double y) {
-        return x + y;
-
-    }
-
-    public static int sub(int x, int y) {
-        return x - y;
-    }
-
-    public static float sub(float x, float y) {
-        return x - y;
-    }
-
-    public static double sub(double x, double y) {
-        return x - y;
-
-    }
-
-
-    public static int div(int x, int y) {
-        return x / y;
-    }
-
-    public static float div(float x, float y) {
-        return x / y;
-    }
-
-    public static double div(double x, double y) {
-        return x / y;
-
-    }
-
-    public static int mul(int x, int y) {
-        return x * y;
-    }
-
-    public static float mul(float x, float y) {
-        return x * y;
-    }
-
-    public static double mul(double x, double y) {
-        return x * y;
-
-    }
-
-    public static int main(int x, int y) {
-        return x + y;
-    }
-
-    public static double main(double x, double y) {
-        return x + y;
-    }
-
-    public static float main(float x, float y) {
-        return x + y;     //=()
-    }
-
 }
 
