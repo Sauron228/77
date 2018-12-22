add2febac2ee0b7fc508bc7d88bb7db016c6e1d8
Sauron228 <burkovskiy_1997@ukr.net>
2018-12-22 20:36:58 +0300

    Initial commit

---
+++ src/hometask1/Calculator.java
@@ -0,0 +1,30 @@
+package hometask1;
+
+public class Calculator {
+    public static int add(int x, int y) {
+        int result = x + y;
+        return result;
+    }
+
+    public static float add(float x, float y) {
+        return x + y;
+
+    }
+
+    public static double add(double x, double y) {
+        return x + y;
+
+    }
+
+    public static int sub(int x, int y) {
+        return x - y;
+    }
+
+    public static int mul(int x, int y) {
+        return x * y;
+    }
+
+    public static int div(int x, int y) {
+        return x / y;
+    }
+}
