1353cae3a41c1792942e76bfbe8d85ecd1a07d6b
Sauron228 <burkovskiy_1997@ukr.net>
2019-01-19 19:52:21 +0300

    унпеу

--- src/hometask4/Сlass3.java
+++ src/hometask4/Сlass3.java
@@ -2,6 +2,10 @@ package hometask4;
 
 public class Сlass3 {
     public static void main(String[] args) {
+                int v1 = 38;
+                int v2 = 42;
+                boolean result = (4 / v1) < (5 / v2);
+                System.out.println(result);
+            }
+        }
 
-    }
-}
