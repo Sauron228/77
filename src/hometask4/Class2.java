ecc27a9d94a7a1b77649ab7f95853840ba77f9eb
Sauron228 <burkovskiy_1997@ukr.net>
2019-01-19 18:11:22 +0300

    унпеу

---
+++ src/hometask4/Class2.java
@@ -0,0 +1,12 @@
+package hometask4;
+
+public class Class2 {
+    public static void main(String[] args) {
+        int width = 5;
+        int length = 5;
+        int sitePerimeter = width + width + length + length;
+        int square = width * length;
+        System.out.println(sitePerimeter);
+        System.out.println(square);
+    }
+}
